9cd8b3f968e7df5d0016982111fd69eeb104dbad
Alik <=ali.mars.99@gmail.com>
2020-09-10 22:05:35 +0600

    finish: project mvp

---
+++ crypto_calculator/core/constants.py
@@ -0,0 +1,7 @@
+USD = 'usd'
+
+ETHEREUM = 'ethereum'
+ETHEREUM_CLASSIC = 'ethereum-classic'
+
+ethereums_ids = [ETHEREUM, ETHEREUM_CLASSIC]
+
--- crypto_calculator/core/views.py
+++ crypto_calculator/core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from core import constants
+from pycoingecko import CoinGeckoAPI
 
-# Create your views here.
+client = CoinGeckoAPI()
+
+
+async def dashboard(request):
+    params = {
+        'localization': 'false',
+        'tickers': 'false',
+        'market_data': 'true',
+        'community_data': 'false',
+        'developer_data': 'false',
+        'sparkline': 'false'
+    }
+
+    ethereum_details = [client.get_coin_by_id(ethereum, **params) for ethereum in constants.ethereums_ids]
+
+    context = {
+        'etheriums_details': ethereum_details,
+    }
+    return render(request, 'base.html', context)
